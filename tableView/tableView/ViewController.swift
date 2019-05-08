485b94a5698d1f9ebdd6c7964e3a595ad1c3b9d4
kenreisan <kenreisan@users.noreply.github.com>
2019-05-08 11:26:02 -0500

    no sirve pero de algo ha de servir

---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by 2019-2 on 5/6/19.
+//  Copyright © 2019 2019-2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController{
+    @IBOutlet weak var labelOne: UILabel!
+    @IBOutlet weak var labelTwo: UILabel!
+    
+    let tvc = TableViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
